70aaa80018e0f5bb417c8efa86f5335618599a07
Bohdan Zhukovskyi <bohda@MacBook-Air.local>
2023-05-29 20:29:51 +0300

    Another one1

--- food_delivery_app/food_delivery_project/settings.py
+++ food_delivery_app/food_delivery_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-894a=%rlasdz1d7@uqzj)ls63qki3_)0d+u=dso+r=j6hjd4xu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'fooddeliveryapp.herokuapp.com']
 
 
 # Application definition
